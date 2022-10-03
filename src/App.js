3773a8f4c16dc27f07ceb9d44d05129173dc451f
rabiulhossen <rhsimon69@gmail.com>
2022-10-03 23:59:53 +0600

    done by

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import './App.css';
-import { render } from "react-dom"
-import { Router } from "@reach/router"
-import Navbar from './components/Shared/Navbar';
-import Home from './components/pages/Home';
-import Services from './components/pages/Services';
-import About from './components/pages/About';
-import Contact from './components/pages/Contact';
+import "./App.css";
+import { Router } from "@reach/router";
+import { useEffect } from "react";
+import { themeChange } from "theme-change";
+import Navbar from "./components/Shared/Navbar";
+import Home from "./components/pages/Home";
+import Services from "./components/pages/Services";
+import About from "./components/pages/About";
+import Contact from "./components/pages/Contact";
 
+const PublicRoute = [
+  { name: "Home", Path:Home },
+  { name: "Services", Path:Services  },
+  { name: "About", Path:About },
+  { name: "Contact", Path:Contact }
+];
+console.log(PublicRoute.Path)
 function App() {
+  useEffect(() => {
+    themeChange(false);
+    // 👆 false parameter is required for react project
+  }, []);
+
   return (
-    <>
-    <Navbar />
-    <Router>
-    <Home path="/"/>
-    <About path="/about"/>
-    <Contact path="/contact"/>
-    <Services path="/services"/>
-    </Router>
-    
-    </>
+    <div className="app">
+      <Navbar />
+      {PublicRoute.map((route) => {
+        <Router key={route.name} path={<route.Path/>}/> ;
+      })}
+    </div>
   );
 }
 
